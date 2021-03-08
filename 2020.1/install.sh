cdc84d60e7a14b072d5b6ae27b8d5f23366bf74a
Tomasz Klim <github@tomaszklim.pl>
2021-03-09 00:02:32 +0100

    no longer need to update /etc/rc.drives

--- 2020.1/install.sh
+++ 2020.1/install.sh
@@ -2,9 +2,7 @@
 
 apt-get install mc htop python-configparser nvme-cli dislocker
 
-if [ -f /etc/rc.drives ]; then
-	cp rc.drives /etc
-else
+if [ ! -f /etc/rc.drives ]; then
 	cp rc.drives /etc
 	cp rc-drives.service /etc/systemd/system
 	systemctl daemon-reload
