58cb8507c99cf348a3b4de9b0b97edf5805fcf17
Tomasz Klim <github@tomaszklim.pl>
2021-04-28 00:51:21 +0200

    add more delay on compulab (fit-pc2, possibly more) devices

--- internal/kali/delay.sh
+++ internal/kali/delay.sh
@@ -5,6 +5,12 @@
 # (eg. on Wifi-only computers, where we don't know the password)
 sleep 15
 
+# additional time on Fit-PC2 and possibly other boards,
+# due to slower Wi-fi autoconfiguration on them
+if [ -d /sys/class/dmi/id ] && grep -q CompuLab /sys/class/dmi/id/board_vendor; then
+	sleep 10
+fi
+
 # additional time to allow smooth start of X environment
 if [ "`ps aux |grep Xorg |grep -v grep`" != "" ]; then
 	sleep 20
