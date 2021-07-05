2a8c17d5e9bdce8878e2cf48c8a0aef7dd9e003d
Tomasz Klim <github@tomaszklim.pl>
2021-07-05 11:27:44 +0200

    improved filtering microsoft reserved partitions

--- internal/kali/get-udev-unrecognized-devices.sh
+++ internal/kali/get-udev-unrecognized-devices.sh
@@ -8,18 +8,18 @@ recognized=`ls -l /dev/disk/by-uuid |grep -v ^total |rev |cut -d'/' -f1 |rev |so
 #
 # 2. grep " 0 part" leaves just partitions (UUID and non-UUID ones)
 #
-# 3. now filter out all partitions with size 128M - the intention
+# 3. now filter out all partitions smaller than 1G - the intention
 # is to remove Microsoft Reserved Partitions, that look like
 # possibly encrypted, and trigger VeraCrypt key search, while
 # not having any useful data
 #
 # https://en.wikipedia.org/wiki/Microsoft_Reserved_Partition
 #
-# this possibly removes also other (UUID) partitions of 128M size,
+# this possibly removes also other (UUID) partitions smaller than 1G,
 # but at this stage we are interested only in non-UUID ones, while
-# UUID partitions are removed in the next step
+# UUID partitions are already processed, and are removed below
 
-lsblk |grep " 0 part" |tr -d '─├└' |grep -v 128M |cut -d' ' -f1 |egrep -v "($recognized)"
+lsblk |grep " 0 part" |egrep -v "(K|M)  0 part" |tr -d '─├└' |cut -d' ' -f1 |egrep -v "($recognized)"
 
 
 # TODO: filter out LUKS-encrypted Linux swap partitions:
