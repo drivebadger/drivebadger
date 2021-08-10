f3fc1774463e0006ca6193a1fafdc33c0f8a6537
Tomasz Klim <github@tomaszklim.pl>
2021-08-10 19:52:00 +0200

    support for computers with mmc storage

--- internal/generic/get-drive-serial.sh
+++ internal/generic/get-drive-serial.sh
@@ -4,17 +4,19 @@ drive=$1
 directory=$2
 
 if [ ! -f $directory/$drive.txt ]; then
-	if [[ $drive =~ ^nvme ]]; then
+	if [[ $drive =~ ^mmc ]]; then
+		udevadm info -a /dev/$drive >$directory/$drive.txt
+	elif [[ $drive =~ ^nvme ]]; then
 		nvme id-ctrl /dev/$drive >$directory/$drive.txt
 	else
 		hdparm -I /dev/$drive >$directory/$drive.txt
 	fi
 fi
 
-if [[ $drive =~ ^nvme ]]; then
-	key=sn
+if [[ $drive =~ ^mmc ]]; then
+	grep 'ATTRS{serial}' $directory/$drive.txt |tr -d ' \t\"' |tr '=' ' ' |awk '{ print $2 }'
+elif [[ $drive =~ ^nvme ]]; then
+	cat $directory/$drive.txt |tr -d ' \t' |grep ^sn: |cut -d':' -f2
 else
-	key=SerialNumber
+	cat $directory/$drive.txt |tr -d ' \t' |grep ^SerialNumber: |cut -d':' -f2
 fi
-
-cat $directory/$drive.txt |tr -d ' \t' |grep ^$key: |cut -d':' -f2
--- internal/generic/get-partition-drive.sh
+++ internal/generic/get-partition-drive.sh
@@ -4,7 +4,7 @@ partition=$1
 
 if [[ $partition =~ ^dm ]] || [[ $partition =~ ^md ]]; then
 	echo $partition  # is it enough to properly support various RAID types?
-elif [[ $partition =~ ^nvme ]]; then
+elif [[ $partition =~ ^mmc ]] || [[ $partition =~ ^nvme ]]; then
 	echo $partition |cut -dp -f1
 else
 	echo $partition |sed 's/[0-9]//g'  # sdx or sdxy
