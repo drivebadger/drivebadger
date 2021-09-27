da1bee49b3fd4a2d9a979644bd45d8135b3f3e95
Tomasz Klim <github@tomaszklim.pl>
2021-09-27 15:31:21 +0200

    init display after boot

--- internal/mobile/events/boot.sh
+++ internal/mobile/events/boot.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 . /opt/drivebadger/internal/mobile/functions
 
+if [ -d /opt/drivebadger/external/ext-mobile-drivers ]; then
+	/opt/drivebadger/external/ext-mobile-drivers/init.sh
+fi
+
 show "ready"
